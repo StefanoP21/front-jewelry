d4e9d2a1b47d66e58d922a5e52bcc7e311f0703d
Oscar Medina <omedina.reyes@hotmail.com>
2024-10-07 18:55:30 -0500

    refactor: all files

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,78 +8,52 @@ import { usePathname } from "next/navigation";
 import { User as UserModel } from "@/core/models";
 
 interface DashboardLayoutProps {
-   children: ReactNode;
+  children: ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-   const pathname = usePathname();
+  const pathname = usePathname();
 
-   const user: UserModel = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")!) : {
-      id: 1,
-      name: "Jhon",
-      lastname: "Doe",
-      dni: "12345678",
-      role: "ADMIN"
-   }
+  const user: UserModel = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user")!)
+    : {
+        id: 1,
+        name: "Jhon",
+        lastname: "Doe",
+        dni: "12345678",
+        role: "ADMIN",
+      };
 
-   return (
-      <div className="flex">
-         {/* Sidebar */}
-         <Sidebar user={user}>
-            <Link href="/">
-               <SidebarItem
-                  icon={<Home size={20} />}
-                  text="Home"
-                  active={pathname === "/"}
-               />
-            </Link>
-            <Link href="/order">
-               <SidebarItem
-                  icon={<ShoppingBasket size={20} />}
-                  text="Ventas"
-                  active={pathname === "/order"}
-               />
-            </Link>
-            <Link href="/product">
-               <SidebarItem
-                  icon={<Gem size={20} />}
-                  text="Productos"
-                  active={pathname === "/product"}
-               />
-            </Link>
-            <Link href="/purchase">
-               <SidebarItem
-                  icon={<Package size={20} />}
-                  text="Compras"
-                  active={pathname === "/purchase"}
-               />
-            </Link>
-            <Link href="/refunds">
-               <SidebarItem
-                  icon={<TicketX size={20} />}
-                  text="Devoluciones"
-                  active={pathname === "/refunds"}
-               />
-            </Link>
-            {
-               user.role === "ADMIN" && (
-                  <Link href="/user">
-                     <SidebarItem
-                        icon={<User size={20} />}
-                        text="Usuarios"
-                        active={pathname === "/user"}
-                     />
-                  </Link>
-               )
-            }
-         </Sidebar>
+  return (
+    <div className="flex">
+      {/* Sidebar */}
+      <Sidebar user={user}>
+        <Link href="/">
+          <SidebarItem icon={<Home size={20} />} text="Home" active={pathname === "/"} />
+        </Link>
+        <Link href="/order">
+          <SidebarItem icon={<ShoppingBasket size={20} />} text="Ventas" active={pathname === "/order"} />
+        </Link>
+        <Link href="/product">
+          <SidebarItem icon={<Gem size={20} />} text="Productos" active={pathname === "/product"} />
+        </Link>
+        <Link href="/purchase">
+          <SidebarItem icon={<Package size={20} />} text="Compras" active={pathname === "/purchase"} />
+        </Link>
+        <Link href="/refunds">
+          <SidebarItem icon={<TicketX size={20} />} text="Devoluciones" active={pathname === "/refunds"} />
+        </Link>
+        {user.role === "ADMIN" && (
+          <Link href="/user">
+            <SidebarItem icon={<User size={20} />} text="Usuarios" active={pathname === "/user"} />
+          </Link>
+        )}
+      </Sidebar>
 
-         {/* Main Content */}
-         <div className="flex-1 p-6">
-            {children}
-         </div>
-      </div>
-   );
+      {/* Main Content */}
+      <div className="flex-1 p-6">{children}</div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
