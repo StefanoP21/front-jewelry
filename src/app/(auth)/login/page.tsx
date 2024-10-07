510e44f65866519af563cc424572aefce42b6532
Stefano Palomino <stefanop21@outlook.es>
2024-10-07 13:47:52 -0500

    refactor: added prettier configuration

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,37 +1,26 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Button } from '@/components/ui/button';
-import { useMutation } from '@tanstack/react-query';
-import { AuthService } from '@/core/services/auth.service';
-import { LoginDto, UserResponse } from '@/core/models';
-import { useAuthStore } from '@/core/store/auth.store';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { AuthService } from "@/core/services/auth.service";
+import { LoginDto, UserResponse } from "@/core/models";
+import { useAuthStore } from "@/core/store/auth.store";
 
 const loginSchema = z.object({
-  dni: z
-    .string()
-    .min(8, { message: 'El DNI debe tener al menos 8 caracteres' }),
-  password: z
-    .string()
-    .min(6, { message: 'La contraseña debe tener al menos 8 caracteres' }),
+  dni: z.string().min(8, { message: "El DNI debe tener al menos 8 caracteres" }),
+  password: z.string().min(6, { message: "La contraseña debe tener al menos 8 caracteres" }),
 });
 
 export default function LoginPage() {
   const form = useForm({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      dni: '',
-      password: '',
+      dni: "",
+      password: "",
     },
   });
 
@@ -44,7 +33,7 @@ export default function LoginPage() {
       console.log(data);
     },
     onError: (error: Error) => {
-      console.error('Error en el login: ', error.message);
+      console.error("Error en el login: ", error.message);
     },
   });
 
@@ -58,10 +47,7 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-4 bg-white p-6 rounded-lg shadow-md w-80"
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 bg-white p-6 rounded-lg shadow-md w-80">
           <h1 className="text-2xl font-bold text-center">Login</h1>
           <FormField
             name="dni"
@@ -99,18 +85,9 @@ export default function LoginPage() {
               </FormItem>
             )}
           />
-          {mutation.isPending && (
-            <p className="text-center text-blue-500">Iniciando sesión</p>
-          )}
-          {mutation.isError && (
-            <p className="text-center text-red-500">
-              {mutation.error?.message}
-            </p>
-          )}
-          <Button
-            type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white"
-          >
+          {mutation.isPending && <p className="text-center text-blue-500">Iniciando sesión</p>}
+          {mutation.isError && <p className="text-center text-red-500">{mutation.error?.message}</p>}
+          <Button type="submit" className="w-full bg-blue-500 hover:bg-blue-600 text-white">
             Iniciar sesión
           </Button>
         </form>
--- src/core/models/index.ts
+++ src/core/models/index.ts
@@ -1,2 +1,2 @@
-export * from './user/dtos';
-export * from './user/model';
+export * from "./user/dtos";
+export * from "./user/model";
